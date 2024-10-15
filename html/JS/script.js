98384bef43045fcfaaf6cf6eb4e27e3a41cadcb0
CraigDavao <92660901+CraigDavao@users.noreply.github.com>
2024-10-15 22:45:47 +0800

    Add files via upload

---
+++ html/JS/script.js
@@ -0,0 +1,49 @@
+// let $firstname = "craig";
+// let $friend = "jimwell";
+// let $her = "she";
+// let $kape = "coffee."
+// alert($firstname + " the friendly neighbor, waved at  " + $friend + " as " + $her + " walked by " + $friend + "'s house. " + $friend + " smiled back and invited " + $firstname + " in for a cup of " + $kape ); 
+
+// document.getElementById("craig").innerHTML = $firstname + " the friendly neighbor, waved at " + $friend + " as " + $her + " walked by " + $friend + "'s house. "  + $friend + " smiled back and invited " + $firstname + " in for a cup of " + $kape; 
+
+// let x = 5;
+// let y = 15;
+
+// alert(y/x+y);
+
+// let $math = 89;
+// let $science = 78;
+// let $pe = 90;
+// let $filipino = 98;
+// let $english = 95;
+
+// let $average = ($math + $science + $pe + $filipino + $english)/5;
+// alert("Math : " +$math + " Science : " +$science + " PE : " + $pe + " filipino : " + $filipino + " english : " + $english);
+// alert( "Average : " +$average);
+
+// String length
+
+// let word = "wow";
+// let len = word.length;
+// console.log (len);
+
+// String Indicates
+
+// let word = "jonathan";
+// alert(word[word.length-1]);
+
+// let craig = "jonathan";
+// craig = craig.toUpperCase();
+// alert(craig);
+
+// let num1 = 5;
+// let num2 = 5;
+// let sum = num1 + num2;
+// alert(`${num1} + ${num2} = ${sum}`);
+
+let self = "Craig";
+let friend = "Jimwell";
+let her = "she";
+let kape = "coffee";
+
+alert(`${self}, the friendly neighbor, waved at ${friend} as ${her} walked by ${friend}'s house. ${friend} smiled back and invited ${self} in for a cup of ${kape}.`)
